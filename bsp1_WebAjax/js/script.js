212adc395193add9c41f761809b44fa4cdf7d87d
FatihCapar <fatih.capar@student.htldornbirn.at>
2023-11-22 22:39:29 +0100

    V.1-3_Plot_fix_Icons

--- bsp1_WebAjax/js/script.js
+++ bsp1_WebAjax/js/script.js
@@ -7,9 +7,9 @@ function get_movie_result(){
   alert (search);
   if (search != null || search != "") {
     $.ajax({
-        url: "http://www.omdbapi.com/?s="+search+"&r=json&apikey=2279b9f1",
-        dataType: 'json',
-        success: function(data) {
+      url: "http://www.omdbapi.com/?s="+search+"&r=json&apikey=2279b9f1",
+      dataType: 'json',
+      success: function(data) {
         if(data.hasOwnProperty("Response") && data.Response =="True" ){
           //ABfrage ist auf jedenfall erfolgreich noch grantie daten ok oder vorhanden
           //data.Search Array von Ergebnisse
@@ -17,13 +17,13 @@ function get_movie_result(){
         }
 
 
-      },   
-        error: function (xmlHttpRequest, textStatus, errorThrown) {
-              alert(textStatus, errorThrown);
-       }
-     }
-     );
-   }
+      },
+      error: function (xmlHttpRequest, textStatus, errorThrown) {
+        alert(textStatus, errorThrown);
+      }
+    }
+    );
+  }
 }
 
 function split_jason(myArray){
@@ -32,15 +32,34 @@ function split_jason(myArray){
     for(var x =0; x < myArray.length; x++){
       var poster= "";
       if(myArray[x].hasOwnProperty("Poster") && myArray[x].Poster != "N/A") poster ="<img class='img-fluid' style='max-width: 100%; height: auto;' src='" + myArray[x].Poster + "'><br>";
-      var newRowContent = "<tr><td>" + myArray[x].Title + "</td><td>" + myArray[x].Year +"<td><td>" + poster +"</td><tr>";
+
+      var plot = "Short Plot"; // Standardwert
+      if (myArray[x].hasOwnProperty("imdbID")) {
+        // Hier wird die Plot-Abfrage mit dem Standardwert "short" durchgeführt
+        $.ajax({
+          url: "http://www.omdbapi.com/?i=" + myArray[x].imdbID + "&plot=short&r=json&apikey=2279b9f1",
+          async: false, // Synchron, um das Ergebnis sofort zu erhalten
+          dataType: 'json',
+          success: function (plotData) {
+            if (plotData.hasOwnProperty("Plot")) {
+              plot = plotData.Plot;
+            }
+          },
+          error: function (xmlHttpRequest, textStatus, errorThrown) {
+            console.error("Error fetching plot data: " + textStatus, errorThrown);
+          }
+        });
+      }
+      
+      var newRowContent = "<tr><td>" + myArray[x].Title + "</td><td>" + myArray[x].Year + "<td><td>" + poster + "</td><td>" + plot + "</td></tr>";
       $("#movieresult tbody").append(newRowContent);
     }
   } else{
-  alert("Fehler bei der Verarbeitung !");
+    alert("Fehler bei der Verarbeitung !");
   }
 }
 function check_Array(myArray) {
-  if(typeof myArray === 'object' && myArray instanceof Array) { 
+  if(typeof myArray === 'object' && myArray instanceof Array) {
     return true;
   } else{
     return false;
