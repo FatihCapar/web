fd84a419fa7c154efa3987f60feb44a1a33aa492
FatihCapar <fatih.capar@student.htldornbirn.at>
2023-11-23 08:26:12 +0100

    V.1-3-1_format

--- bsp1_WebAjax/js/script.js
+++ bsp1_WebAjax/js/script.js
@@ -1,37 +1,35 @@
 //
-var dataFound=0;
-var dataPerPage=10;
+var dataFound = 0;
+var dataPerPage = 10;
 //
-function get_movie_result(){
+
+function get_movie_result() {
   var search = $("#search").val();
-  alert (search);
+  alert(search);
   if (search != null || search != "") {
     $.ajax({
-      url: "http://www.omdbapi.com/?s="+search+"&r=json&apikey=2279b9f1",
+      url: "http://www.omdbapi.com/?s=" + search + "&r=json&apikey=2279b9f1",
       dataType: 'json',
-      success: function(data) {
-        if(data.hasOwnProperty("Response") && data.Response =="True" ){
-          //ABfrage ist auf jedenfall erfolgreich noch grantie daten ok oder vorhanden
-          //data.Search Array von Ergebnisse
+      success: function (data) {
+        if (data.hasOwnProperty("Response") && data.Response == "True") {
+          // Abfrage ist auf jeden Fall erfolgreich, noch Garantie, dass Daten OK oder vorhanden sind
+          // data.Search Array von Ergebnisse
           split_jason(data.Search);
         }
-
-
       },
       error: function (xmlHttpRequest, textStatus, errorThrown) {
         alert(textStatus, errorThrown);
       }
-    }
-    );
+    });
   }
 }
 
-function split_jason(myArray){
-  if(check_Array(myArray)) {
+function split_jason(myArray) {
+  if (check_Array(myArray)) {
     $("#movieresult tbody").empty();
-    for(var x =0; x < myArray.length; x++){
-      var poster= "";
-      if(myArray[x].hasOwnProperty("Poster") && myArray[x].Poster != "N/A") poster ="<img class='img-fluid' style='max-width: 100%; height: auto;' src='" + myArray[x].Poster + "'><br>";
+    for (var x = 0; x < myArray.length; x++) {
+      var poster = "";
+      if (myArray[x].hasOwnProperty("Poster") && myArray[x].Poster != "N/A") poster = "<img class='img-fluid' style='max-width: 100%; height: auto;' src='" + myArray[x].Poster + "'><br>";
 
       var plot = "Short Plot"; // Standardwert
       if (myArray[x].hasOwnProperty("imdbID")) {
@@ -50,18 +48,19 @@ function split_jason(myArray){
           }
         });
       }
-      
+
       var newRowContent = "<tr><td>" + myArray[x].Title + "</td><td>" + myArray[x].Year + "<td><td>" + poster + "</td><td>" + plot + "</td></tr>";
       $("#movieresult tbody").append(newRowContent);
     }
-  } else{
+  } else {
     alert("Fehler bei der Verarbeitung !");
   }
 }
+
 function check_Array(myArray) {
-  if(typeof myArray === 'object' && myArray instanceof Array) {
+  if (typeof myArray === 'object' && myArray instanceof Array) {
     return true;
-  } else{
+  } else {
     return false;
   }
 }
