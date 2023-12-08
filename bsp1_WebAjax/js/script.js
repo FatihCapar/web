67a437137d22f33689c76dce687a8636ff54cdfb
FatihCapar <fatih.capar@student.htldornbirn.at>
2023-12-08 16:51:12 +0100

    V.2_0-Favoritenliste_page_Logo

--- bsp1_WebAjax/js/script.js
+++ bsp1_WebAjax/js/script.js
@@ -1,20 +1,75 @@
-//
 var dataFound = 0;
 var dataPerPage = 10;
-//
+var favoriteMovies = [];
+var currentPage = 1;
+var totalResults = 0;
+var searchResults = [];
+
+$(document).ready(function() {
+  loadFavorites();
+});
+
+
+function showResults() {
+  var startIndex = (currentPage - 1) * dataPerPage;
+  var endIndex = startIndex + dataPerPage;
+  var resultsToDisplay = searchResults.slice(startIndex, endIndex);
+
+  split_jason(resultsToDisplay);
+}
+
+async function nextPage() {
+  
+  if ((currentPage * dataPerPage) < totalResults) {
+    currentPage++;
+    await loadResultsPage(currentPage);
+    showResults();
+  }
+}
+
+async function loadResultsPage(page) {
+  var search = $("#search").val();
+  if (search != null || search != "") {
+    try {
+      var response = await $.ajax({
+        url: "http://www.omdbapi.com/?s=" + search + "&r=json&apikey=2279b9f1&page=" + page,
+        dataType: 'json'
+      });
+      if (response.hasOwnProperty("Response") && response.Response == "True") {
+        totalResults = parseInt(response.totalResults) || 0;
+        searchResults = searchResults.concat(response.Search || []);
+      } else {
+        console.error("Error in API response:", response.Error);
+      }
+    } catch (error) {
+      console.error("Error making API request:", error);
+    }
+  }
+}
+
+
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    showResults();
+  }
+}
+
 
 function get_movie_result() {
+  currentPage = 1;
   var search = $("#search").val();
-  alert(search);
   if (search != null || search != "") {
     $.ajax({
       url: "http://www.omdbapi.com/?s=" + search + "&r=json&apikey=2279b9f1",
       dataType: 'json',
       success: function (data) {
         if (data.hasOwnProperty("Response") && data.Response == "True") {
-          // Abfrage ist auf jeden Fall erfolgreich, noch Garantie, dass Daten OK oder vorhanden sind
-          // data.Search Array von Ergebnissen
-          split_jason(data.Search);
+          totalResults = parseInt(data.totalResults) || 0;
+          searchResults = data.Search || [];
+          
+          showResults();
         }
       },
       error: function (xmlHttpRequest, textStatus, errorThrown) {
@@ -24,72 +79,51 @@ function get_movie_result() {
   }
 }
 
-function split_jason(myArray) {
+async function split_jason(myArray) {
   if (check_Array(myArray)) {
     $("#movieresult tbody").empty();
     for (var x = 0; x < myArray.length; x++) {
       var poster = "";
-      var plot = "N/A"; // Standardwert
-      var rating = "N/A"; // Standardwert für Bewertung
-      var genre = "N/A"; // Standardwert für Genre
-      var year = "N/A"; // Standardwert für Jahr
+      var plot = "N/A";
+      var rating = "N/A";
+      var genre = "N/A";
+      var year = "N/A";
 
       if (myArray[x].hasOwnProperty("Poster") && myArray[x].Poster != "N/A") poster = "<img class='img-fluid' style='max-width: 100%; height: auto;' src='" + myArray[x].Poster + "'><br>";
 
       if (myArray[x].hasOwnProperty("imdbID")) {
-        // Hier wird die Plot-Abfrage mit dem Standardwert "short" durchgeführt
-        $.ajax({
-          url: "http://www.omdbapi.com/?i=" + myArray[x].imdbID + "&plot=short&r=json&apikey=2279b9f1",
-          async: false, // Synchron, um das Ergebnis sofort zu erhalten
-          dataType: 'json',
-          success: function (plotData) {
-            if (plotData.hasOwnProperty("Plot")) {
-              plot = plotData.Plot;
-            }
-          },
-          error: function (xmlHttpRequest, textStatus, errorThrown) {
-            console.error("Error fetching plot data: " + textStatus, errorThrown);
+        await getPlotData(myArray[x].imdbID).then((plotData) => {
+          if (plotData.hasOwnProperty("Plot")) {
+            plot = plotData.Plot;
           }
+        }).catch((error) => {
+          console.error("Error fetching plot data: ", error);
         });
 
-        // Hier wird die Bewertungs-Abfrage durchgeführt
-        $.ajax({
-          url: "http://www.omdbapi.com/?i=" + myArray[x].imdbID + "&r=json&apikey=2279b9f1",
-          async: false,
-          dataType: 'json',
-          success: function (ratingData) {
-            if (ratingData.hasOwnProperty("imdbRating")) {
-              rating = ratingData.imdbRating;
-            }
-          },
-          error: function (xmlHttpRequest, textStatus, errorThrown) {
-            console.error("Error fetching rating data: " + textStatus, errorThrown);
+        await getRatingData(myArray[x].imdbID).then((ratingData) => {
+          if (ratingData.hasOwnProperty("imdbRating")) {
+            rating = ratingData.imdbRating;
           }
+        }).catch((error) => {
+          console.error("Error fetching rating data: ", error);
         });
 
-        // Hier wird die Genre- und Jahr-Abfrage durchgeführt
-        $.ajax({
-          url: "http://www.omdbapi.com/?i=" + myArray[x].imdbID + "&r=json&apikey=2279b9f1",
-          async: false,
-          dataType: 'json',
-          success: function (detailData) {
-            if (detailData.hasOwnProperty("Genre")) {
-              genre = detailData.Genre;
-            }
-            if (detailData.hasOwnProperty("Year")) {
-              year = detailData.Year;
-            }
-          },
-          error: function (xmlHttpRequest, textStatus, errorThrown) {
-            console.error("Error fetching detail data: " + textStatus, errorThrown);
+        await getDetailData(myArray[x].imdbID).then((detailData) => {
+          if (detailData.hasOwnProperty("Genre")) {
+            genre = detailData.Genre;
           }
+          if (detailData.hasOwnProperty("Year")) {
+            year = detailData.Year;
+          }
+        }).catch((error) => {
+          console.error("Error fetching detail data: ", error);
         });
       }
 
-      // Hier wird die Bewertung in Sterne umgewandelt
       var starRating = convertToStars(rating);
 
-      var newRowContent = "<tr><td>" + poster + "</td><td>" + starRating + "</td><td>" + plot + "</td><td>" + genre + "</td><td>" + year + "</td><td>" + myArray[x].Title  + "</td></tr>";
+      var newRowContent = "<tr><td>" + poster + "</td><td>" + starRating + "</td><td>" + plot + "</td><td>" + genre + "</td><td>" + year + "</td><td><i class='bi bi-heart-fill' onclick='addToFavorites(\"" + myArray[x].imdbID + "\")'></i> " + myArray[x].Title + "</td></tr>";
+
       $("#movieresult tbody").append(newRowContent);
     }
   } else {
@@ -98,11 +132,7 @@ function split_jason(myArray) {
 }
 
 function check_Array(myArray) {
-  if (typeof myArray === 'object' && myArray instanceof Array) {
-    return true;
-  } else {
-    return false;
-  }
+  return Array.isArray(myArray);
 }
 
 function convertToStars(rating) {
@@ -112,19 +142,230 @@ function convertToStars(rating) {
   var starRating = "";
 
   for (var i = 0; i < fullStars; i++) {
-    starRating += "<i class='bi bi-star-fill'></i>"; // Bootstrap Icons für vollen Stern
+    starRating += "<i class='bi bi-star-fill'></i>";
   }
 
   if (halfStar) {
-    starRating += "<i class='bi bi-star-half'></i>"; // Bootstrap Icons für halben Stern
+    starRating += "<i class='bi bi-star-half'></i>";
   }
-
-  // Leere Sterne
-//  for (var j = 0; j < 5 - fullStars; j++) {
-//    starRating += "<i class='bi bi-star'></i>";
-//  }
 
   return starRating;
 }
+
+function addToFavorites(imdbID) {
+  if (favoriteMovies.indexOf(imdbID) === -1) {
+    favoriteMovies.push(imdbID);
+    saveFavorites();
+    alert("Film zu Favoriten hinzugefügt!");
+  } else {
+    alert("Film ist bereits in den Favoriten!");
+  }
+}
+
+function saveFavorites() {
+  localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+}
+
+function loadFavorites() {
+  var storedFavorites = localStorage.getItem('favoriteMovies');
+  if (storedFavorites) {
+    favoriteMovies = JSON.parse(storedFavorites);
+  }
+}
+
+async function getPlotData(imdbID) {
+  try {
+    return await $.ajax({
+      url: "http://www.omdbapi.com/?i=" + imdbID + "&plot=short&r=json&apikey=2279b9f1",
+      dataType: 'json'
+    });
+  } catch (error) {
+    console.error("Error fetching plot data: ", error);
+    return {};
+  }
+}
+
+async function getRatingData(imdbID) {
+  try {
+    return await $.ajax({
+      url: "http://www.omdbapi.com/?i=" + imdbID + "&r=json&apikey=2279b9f1",
+      dataType: 'json'
+    });
+  } catch (error) {
+    console.error("Error fetching rating data: ", error);
+    return {};
+  }
+}
+
+async function getDetailData(imdbID) {
+  try {
+    return await $.ajax({
+      url: "http://www.omdbapi.com/?i=" + imdbID + "&r=json&apikey=2279b9f1",
+      dataType: 'json'
+    });
+  } catch (error) {
+    console.error("Error fetching detail data: ", error);
+    return {};
+  }
+}
+
+function showFavorites() {
+  $("#favoritesList").empty();
+
+  async function loadDetails(imdbID) {
+    try {
+      return await $.ajax({
+        url: "http://www.omdbapi.com/?i=" + imdbID + "&plot=short&r=json&apikey=2279b9f1",
+        dataType: 'json'
+      });
+    } catch (error) {
+      console.error("Error fetching details for favorite movie: ", error);
+      return {};
+    }
+  }
+
+  async function addToFavoritesList(imdbID) {
+    var favoriteMovie = favoriteMovies.find(function(movie) {
+      return movie === imdbID;
+    });
+  
+    if (favoriteMovie) {
+      var details = await loadDetails(imdbID);
+  
+      var starRating = convertToStars(details.imdbRating || 'N/A');
+      var userStarRating = getUserStarRating(imdbID);
+
+      var starRatingInput =
+      "<div class='mb-3'>" +
+        "<label for='userStarRating_" + imdbID + "' class='form-label'>Deine Bewertung:</label>" +
+        "<select class='form-select' id='userStarRating_" + imdbID + "'>" +
+          "<option value='1'>1 Stern</option>" +
+          "<option value='2'>2 Sterne</option>" +
+          "<option value='3'>3 Sterne</option>" +
+          "<option value='4'>4 Sterne</option>" +
+          "<option value='5'>5 Sterne</option>" +
+          "<option value='6'>6 Sterne</option>" +
+          "<option value='7'>7 Sterne</option>" +
+          "<option value='8'>8 Sterne</option>" +
+          "<option value='9'>9 Sterne</option>" +
+          "<option value='10'>10 Sterne</option>" +
+        "</select>" +
+        "<button class='btn btn-primary mt-2' onclick='submitUserRating(\"" + imdbID + "\")'>Bewertung abschicken</button>" +
+      "</div>";
+
+      // (#8c9287)
+      var listItem =
+      "<li class='mb-3 border rounded p-3' style='background-color: #B3B3B3;'>" +
+        "<div class='row align-items-center'>" +
+          "<div class='col-2 border-right text-center'><strong>Poster:</strong> <img src='" + (details.Poster || 'N/A') + "' alt='Poster' class='img-fluid'></div>" +
+          "<div class='col-1 border-right text-center'><strong>Bewertung:</strong> <span class='text-warning font-weight-bold'>" + (details.imdbRating || 'N/A') + "</span> " + starRating + "</div>" +
+          "<div class='col-1 border-right text-center'><strong>Deine Bewertung:</strong> " + userStarRating + "</div>" +
+          "<div class='col-md-3 border-right'><strong>Plot:</strong> <textarea class='form-control' rows='5' readonly style='background-color: #fff; font-weight: bold;'>" + (details.Plot || 'N/A') + "</textarea></div>" +
+          "<div class='col-2 border-right text-center'><strong>Genre:</strong> " + generateGenreBadges(details.Genre) + "</div>" +
+          "<div class='col-2 border-right text-center'><strong>Year:</strong> " + generateYearBadges(details.Year) + "</div>" +
+          "<div class='col-2 border-right'><strong>Titel:</strong> <span class='text-primary font-weight-bold'>" + (details.Title || 'N/A') + "</span></div>" +
+          "<div class='col-1 text-center'><button class='btn btn-danger btn-sm mt-2' onclick='removeFromFavorites(\"" + imdbID + "\")'>Entfernen</button></div>" +
+          "<div class='col-12'>" + starRatingInput + "</div>" +
+        "</div>" +
+      "</li>";
+  
+function generateGenreBadges(genre) {
+  if (!genre) return 'N/A';
+
+  var genreArray = genre.split(', ');
+  var badges = genreArray.map(function (genreItem) {
+    return "<span class='badge bg-primary mr-1'>" + genreItem + "</span>";
+  });
+
+  return badges.join('');
+}
+
+function generateYearBadges(year) {
+  if (!year) return 'N/A';
+
+  var yearArray = year.split(', ');
+  var badges = yearArray.map(function (yearItem) {
+    return "<span class='badge bg-secondary mr-1'>" + yearItem + "</span>";
+  });
+
+  return badges.join('');
+}
+
+
+      $("#favoritesList").append(listItem);
+    }
+  }
+
+  for (var i = 0; i < favoriteMovies.length; i++) {
+    var imdbID = favoriteMovies[i];
+    addToFavoritesList(imdbID);
+  }
+}
+
+function removeFromFavorites(imdbID) {
+  var index = favoriteMovies.indexOf(imdbID);
+  if (index !== -1) {
+    favoriteMovies.splice(index, 1);
+    saveFavorites();
+    showFavorites();
+    alert("Film aus Favoriten entfernt!");
+  } else {
+    alert("Film nicht in den Favoriten gefunden!");
+  }
+}
+
+function clearFavorites() {
+  favoriteMovies = [];
+  saveFavorites(); 
+  showFavorites(); 
+  alert("Alle Filme aus Favoriten entfernt!");
+}
+function submitUserRating(imdbID) {
+  var userStarRating = $("#userStarRating_" + imdbID).val();
+
+  alert("Deine Bewertung für " + imdbID + ": " + userStarRating);
+
+  saveUserRating(imdbID, userStarRating);
+
+  $("#myModal").modal("hide");
+}
+
+function saveUserRating(imdbID, rating) {
+  localStorage.setItem('userStarRating_' + imdbID, rating);
+}
+
+
+function getUserStarRating(imdbID) {
+  var rating = localStorage.getItem('userStarRating_' + imdbID);
+
+  if (rating !== null) {
+    return convertToStars(rating);
+  } else {
+    return 'N/A';
+  }
+}
+function clearAllFavorites() {
+  favoriteMovies = [];
+  saveFavorites();
+  showFavorites();
+  alert("Alle Filme aus Favoriten entfernt!");
+}
+
+function resetAllRatings() {
+  for (var i = 0; i < favoriteMovies.length; i++) {
+      var imdbID = favoriteMovies[i];
+      localStorage.removeItem('userStarRating_' + imdbID);
+  }
+  showFavorites(); 
+  alert("Alle Bewertungen zurückgesetzt!");
+}
+
+
+
+
+
+
+
+
 
 
